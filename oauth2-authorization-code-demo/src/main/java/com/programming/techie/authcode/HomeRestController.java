1a996e658787202907ae933f32b47a0176ffd123
lofidewanto <lofidewanto@gmail.com>
2023-12-12 13:41:25 +0100

    Add AccessToken OAuth2

--- oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/HomeRestController.java
+++ oauth2-authorization-code-demo/src/main/java/com/programming/techie/authcode/HomeRestController.java
@@ -1,6 +1,8 @@
 package com.programming.techie.authcode;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeRestController {
     
     @GetMapping("/hello")
-    public String hello(Authentication authentication) {
-        return "hello " + getName(authentication) ;
+    public String hello(Authentication authentication, 
+        @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+        String accessToken = authorizedClient.getAccessToken().getTokenValue();
+        return "hello " + getName(authentication) + " - " + accessToken;
     }
 
     private String getName(Authentication authentication) {
